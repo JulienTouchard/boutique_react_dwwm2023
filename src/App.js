3c1b8723dadb5b292f954f6d054d3cf104cda3dc
JulienTouchard <julien.touchard@free.fr>
2023-11-23 14:47:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Gallery from './components/Gallery/Gallery';
+import { articles } from './articles.js';
+import './App.css';
+
+
+
+const App = () => {
+  const [state, setState] = React.useState(
+    {
+      'articles': articles
+    }
+  )
+  return (
+    <>
+    <header></header>
+    <main>
+      <Gallery articles={state.articles}></Gallery>
+    </main>
+    <footer></footer>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/articles.js
@@ -0,0 +1,66 @@
+const articles = [
+    {
+        id: 0,
+        name: "Lapinou",
+        img: "./assets/img/costumes/lapin.jpg",
+        price: 400.00,
+        promo: true,
+        qte: 10,
+        description: "Costume 100% pure laine vierge. Anti allergène. Existe en plusieurs coloris. De grands moments pour vos soirées ..."
+    },
+    {
+        id: 1,
+        name: "Canard",
+        img: "./assets/img/costumes/shopping.webp",
+        price: 350.00,
+        promo: false,
+        qte: 5,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 2,
+        name: "Chaton",
+        img: "./assets/img/costumes/aerobic.jpg",
+        price: 500.00,
+        promo: true,
+        qte: 8,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 3,
+        name: "Mario",
+        img: "./assets/img/costumes/mario.jpg",
+        price: 500.00,
+        promo: true,
+        qte: 15,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 4,
+        name: "Los pollos hermanos",
+        img: "./assets/img/costumes/poulet.jpg",
+        price: 500.00,
+        promo: true,
+        qte: 7,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 5,
+        name: "Sushi",
+        img: "./assets/img/costumes/sushi.jpg",
+        price: 500.00,
+        promo: true,
+        qte: 9,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+    {
+        id: 6,
+        name: "Queen Mum",
+        img: "./assets/img/costumes/queen.jpg",
+        price: 500.00,
+        promo: true,
+        qte: 20,
+        description: "Costume 100% pure laine vierge. blabla"
+    },
+];
+export {articles}
